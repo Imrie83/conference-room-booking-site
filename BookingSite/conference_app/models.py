493a4b71d8d1a4a5459fc78b1f2f8035e0fdab03
Imrie83 <czolgista83@gmail.com>
2021-11-13 19:03:34 +0100

    created methods in Room model allowing to find if room is reserved today as well as filter out all reservations from the past including today

--- BookingSite/conference_app/models.py
+++ BookingSite/conference_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class Room(models.Model):
@@ -7,9 +8,16 @@ class Room(models.Model):
     # availability = models.BooleanField(null=False, default=True)
     projector = models.BooleanField(null=False, default=False)
 
-    def reserved(self):
+
+    def reserved_today(self):
         today = datetime.now().strftime('%Y-%m-%d')
-        self.reservation_set.filter(today)
+        reserved_today = self.reservation_set.filter(date=today)
+        return reserved_today
+
+    def reservation_list(self):
+        today = datetime.now().strftime('%Y-%m-%d')
+        reservation_list = self.reservation_set.filter(date__gt=today)
+        return reservation_list
 
     def __str__(self):
         return f'{self.name} / {self.capacity}'
