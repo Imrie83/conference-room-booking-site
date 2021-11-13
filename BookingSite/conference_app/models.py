4b8f3f1aca640b1c6f924f73ce3e489d50075ff0
Imrie83 <czolgista83@gmail.com>
2021-11-13 11:21:46 +0100

    created a model storing reservations info

--- BookingSite/conference_app/models.py
+++ BookingSite/conference_app/models.py
@@ -6,3 +6,13 @@ class Room(models.Model):
     capacity = models.IntegerField()
     availability = models.BooleanField(null=False, default=True)
     projector = models.BooleanField(null=False, default=False)
+
+
+class Reserve(models.Model):
+    date = models.DateField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False)
+    comment = models.CharField(max_length=250)
+
+    class Meta:
+        unique_together = ('date', 'id')
+
