80d48babc4ecceed9cf7e156f913fb5faaca6af5
Imrie83 <czolgista83@gmail.com>
2021-11-13 14:59:45 +0100

    created a view displaying room details

--- BookingSite/conference_app/views.py
+++ BookingSite/conference_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, response
 from django.views import View
-from .models import Room
+from .models import Room, Reservation
 
 
 class AddRoomView(View):
@@ -37,20 +37,20 @@ class ListAllRoomsView(View):
 
 
 class DeleteRoomView(View):
-    def get(self, id):
+    def get(self, request, id):
         Room.objects.get(id=id).delete()
         return render(request, 'conference_app/delete_room.html', {'message': 'Room has been deleted'})
 
 
 class ModifyRoomView(View):
-    def get(self, id):
+    def get(self, request, id):
         room = Room.objects.get(id=id)
         if room:
             return render(request, 'conference_app/edit_room.html', {'room': room})
         else:
             return render(request, 'conference_app/edit_room.html', {'message': 'Room not found'})
 
-    def post(self, id):
+    def post(self, request, id):
         update_room = Room.object.get(id=id)
 
         room_name = request.POST.get('name')
@@ -59,8 +59,8 @@ class ModifyRoomView(View):
 
         if room_name:
             if not room_capacity > 0:
-                return render(request, 'conference_app/add_room.html', {
-                    'message': 'Room capacity has to be greater than 0'
+                return render(request, 'conference_app/edit_room.html', {
+                    'message': 'Room capacity has to be greater than 0', 'room': update_room
                 })
             try:
                 update_room.name = room_name
@@ -69,16 +69,27 @@ class ModifyRoomView(View):
                 update_room.save()
             except Exception as e:
                 print(e)
-                return render(request, 'conference_app/add_room.html', {'message': 'Room with this name exist'})
+                return render(request, 'conference_app/edit_room.html', {
+                    'message': 'Room with this name exist', 'room': update_room
+                })
         else:
-            return render(request, 'conference_app/add_room.html', {'message': 'Add room name'})
+            return render(request, 'conference_app/edit_room.html', {'message': 'Add room name', 'room': update_room})
 
-        return render(request, 'conference_app/add_room.html', {'message': 'Room has been modified!'})
+        return render(request, 'conference_app/edit_room.html', {'message': 'Room has been modified!', 'room': update_room})
 
 
 class ReserveRoomView(View):
-    def get(self, id):
-        pass
+    def get(self, request, id):
+        room = Room.objects.get(id=id)
+        return render(request, 'conference_app/reserve_room.html', {'room': room})
 
-    def post(self, id):
-        pass
+    # def post(self, request, id):
+    #     date = response.POST['date']
+    #     comment = response.POST['comment']
+    #     reservation = R
+
+class DetailedView(View):
+    def get(self, request, id):
+        room = Room.objects.get(id=id)
+        reservation = Reservation.objects.filter(room=id)
+        return render(request, 'conference_app/details.html', {'room': room, 'reservations': reservation})
