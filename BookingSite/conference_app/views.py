861fe526bc31f8fc849fc25f63c46c6622552192
Imrie83 <czolgista83@gmail.com>
2021-11-13 09:35:37 +0100

    modified view/html template to include a projector bool field

--- BookingSite/conference_app/views.py
+++ BookingSite/conference_app/views.py
@@ -9,10 +9,9 @@ class AddRoomView(View):
         return render(request, 'conference_app/add_room.html')
 
     def post(self, request):
-        # errors = ''
         room_name = request.POST.get('name')
         room_capacity = int(request.POST['capacity'])
-        availability = bool(request.POST.get('availability'))
+        projector = bool(request.POST.get('projector'))
 
         if room_name:
             if not room_capacity > 0:
@@ -20,10 +19,18 @@ class AddRoomView(View):
                     'message': 'Room capacity has to be greater than 0'
                 })
             try:
-                new_room = Room.objects.create(name=room_name, capacity=room_capacity, availability=availability)
+                new_room = Room.objects.create(name=room_name, capacity=room_capacity, projector=projector)
             except Exception:
                 return render(request, 'conference_app/add_room.html', {'message': 'Room already exist'})
         else:
             return render(request, 'conference_app/add_room.html', {'message': 'Add room name'})
 
         return render(request, 'conference_app/add_room.html', {'message': 'Room has been added!'})
+
+
+    class ListAllRoomsView(View):
+        def get(self, request):
+            pass
+
+        def get(self, request):
+            pass
