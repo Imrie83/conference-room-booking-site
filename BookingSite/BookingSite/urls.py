e60d651e592d895f5b37c6750037854ce05a633b
Imrie83 <czolgista83@gmail.com>
2021-11-13 10:20:30 +0100

    add urls (not views ready yet)

--- BookingSite/BookingSite/urls.py
+++ BookingSite/BookingSite/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from conference_app.views import AddRoomView
+from conference_app.views import (
+    AddRoomView,
+    ListAllRoomsView,
+    ModifyRoomView,
+    ReserveRoomView,
+    DeleteRoomView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^room/new/$', AddRoomView.as_view(), name='add-room')
+    re_path(r'^room/new/$', AddRoomView.as_view(), name='add-room'),
+    re_path(r'^room/modify/(?P<id>\d+)/$', ModifyRoomView.as_view(), name='modify-room'),
+    re_path(r'^room/reserve/(?P<id>\d+)/$', ReserveRoomView.as_view(), name='reserve-room'),
+    re_path(r'^room/delete/(?P<id>\d+)/$', DeleteRoomView.as_view(), name='delete-room'),
+    re_path(r'^rooms/$', ListAllRoomsView.as_view(), name='all-rooms'),
 ]
