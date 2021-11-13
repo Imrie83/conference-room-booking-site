4d378b53a9e465440c7fb0f6ea1aaa9721112541
Imrie83 <czolgista83@gmail.com>
2021-11-13 14:58:22 +0100

    changed model name and added __str__ method

--- BookingSite/conference_app/models.py
+++ BookingSite/conference_app/models.py
@@ -2,17 +2,22 @@ from django.db import models
 
 
 class Room(models.Model):
-    name = models.TextField(max_length=25, unique=True)
+    name = models.CharField(max_length=25, unique=True)
     capacity = models.IntegerField()
     availability = models.BooleanField(null=False, default=True)
     projector = models.BooleanField(null=False, default=False)
 
+    def __str__(self):
+        return f'{self.name} / {self.capacity}'
 
-class Reserve(models.Model):
+class Reservation(models.Model):
     date = models.DateField()
     room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False)
     comment = models.CharField(max_length=250)
 
     class Meta:
         unique_together = ('date', 'id')
+
+    def __str__(self):
+        return f'{self.room} - {self.date}'
 
