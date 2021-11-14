bb7c2741f1e391409c41add31af49998ee3208f5
Imrie83 <czolgista83@gmail.com>
2021-11-14 18:41:08 +0100

    added necessary docstrings

--- BookingSite/conference_app/models.py
+++ BookingSite/conference_app/models.py
@@ -3,18 +3,35 @@ from datetime import datetime
 
 
 class Room(models.Model):
+    """
+    Model stores name, capacity and projector availability
+    of a room
+    """
     name = models.CharField(max_length=25, unique=True)
     capacity = models.IntegerField()
-    # availability = models.BooleanField(null=False, default=True)
     projector = models.BooleanField(null=False, default=False)
 
 
     def reserved_today(self):
+        """
+        Based on current date filters object
+        reservations equal to the the date
+
+        :return:
+        a list of reservations equal to today date
+        """
         today = datetime.now().strftime('%Y-%m-%d')
         reserved_today = self.reservation_set.filter(date=today)
         return reserved_today
 
     def reservation_list(self):
+        """
+        Takes today date and only leaves reservations
+        newer than today
+
+        :return:
+        a list of all reservations newer than today
+        """
         today = datetime.now().strftime('%Y-%m-%d')
         reservation_list = self.reservation_set.filter(date__gt=today)
         return reservation_list
@@ -23,6 +40,9 @@ class Room(models.Model):
         return f'{self.name} / {self.capacity}'
 
 class Reservation(models.Model):
+    """
+    Model stores reservations with date, room id and comments
+    """
     date = models.DateField()
     room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False)
     comment = models.CharField(max_length=250)
--- BookingSite/conference_app/views.py
+++ BookingSite/conference_app/views.py
@@ -7,19 +7,43 @@ from django.contrib import messages
 
 
 class AddRoomView(View):
+    """
+    Display form allowing to add a room to database.
+    If conditions met create an instance of :model: `conference_app.Room
+    and save it to database.
+
+    **Context**
+
+    ``new_room``
+        An instance of :model:`conference_app.Room`.
+
+    ``room_name``
+        A str variable loaded from template
+
+    ``room_capacity``
+        An int variable taken from template
+
+    ``projector``
+        A bool variable taken from the template
+
+    **Template:**
+
+    :template:`conference_app/add_room.html`
+    """
     def get(self, request):
         return render(request, 'conference_app/add_room.html')
 
     def post(self, request):
+        # Load variables from a from in template
         room_name = request.POST.get('name')
         room_capacity = int(request.POST['capacity'])
         projector = bool(request.POST.get('projector'))
 
-        if room_name:
-            if not room_capacity > 0:
+        if room_name:                   # if room name not empty...
+            if not room_capacity > 0:   # room capacity cannot be lower than 1
                 messages.error(request, 'Capacity has to be greater than 0')
                 return redirect("/")
-            try:
+            try:    # create an instance of a model Room, if fails return and pass to template error messages
                 new_room = Room.objects.create(name=room_name, capacity=room_capacity, projector=projector)
             except Exception:
                 messages.error(request, 'Room already exist')
@@ -33,21 +57,67 @@ class AddRoomView(View):
 
 
 class ListAllRoomsView(View):
+    """
+    Display all elements from :model:`conference_app.Room`.
+
+    **Context**
+
+    ``rooms``
+        An instance of :model:`conference_app.Room`.
+
+    **Template:**
+
+    :template:`conference_app/list_rooms.html`
+    """
     def get(self, request):
         rooms = Room.objects.all()
-        if not rooms:
-            messages.success(request, 'There are no rooms in database')
+        if not rooms:   # If no rooms in database return error
+            messages.error(request, 'There are no rooms in database')
             return redirect("/")
         return render(request, 'conference_app/list_rooms.html', {'rooms': rooms})
 
 
 class DeleteRoomView(View):
+    """
+    Based on id deletes an element from :model:`conference_app.Room`.
+
+    **Context**
+
+    **Template:**
+
+    """
     def get(self, request, id):
         Room.objects.get(id=id).delete()
         return redirect("/")
 
 
 class ModifyRoomView(View):
+    """
+    Display form allowing to modify rooms,
+    creates an instance of :model: `conference_app.Room
+    and saves it to database if conditions met
+
+    **Context**
+
+    ``room``
+        An instance of :model:`conference_app.Room`.
+
+    ``update_room``
+        An instance of :model: `conference_app.Room`.
+
+    ``room_name``
+        A str variable loaded from template
+
+    ``room_capacity``
+        An int variable taken from template
+
+    ``projector``
+        A bool variable taken from the template
+
+    **Template:**
+
+    :template:`conference_app/edit_room.html`
+    """
     def get(self, request, id):
         room = Room.objects.get(id=id)
         if room:
@@ -81,6 +151,28 @@ class ModifyRoomView(View):
 
 
 class ReserveRoomView(View):
+    """
+    Creates and saves a new object of :model: conference_app.Reservation
+
+    **Context**
+
+    ``room``
+       An instance of :model:`conference_app.Room`.
+
+    ``reservations_check``
+       An instance of :model: `conference_app.Reservations`.
+
+    ``date``
+        A date variable loaded from template form.
+
+    ``comment``
+        A str variable loaded from template form.
+
+
+    **Template:**
+
+    :template:`conference_app/edit_room.html`
+    """
     def get(self, request, id):
         room = Room.objects.get(id=id)
         return render(request, 'conference_app/reserve_room.html', {'room': room})
@@ -106,6 +198,18 @@ class ReserveRoomView(View):
 
 
 class DetailedView(View):
+    """
+    Creates an instance of :model: conference_app.Room
+
+    **Context**
+
+    ``room``
+       An instance of :model:`conference_app.Room`.
+
+    **Template:**
+
+    :template:`conference_app/details.html`
+    """
     def get(self, request, id):
         room = Room.objects.get(id=id)
         return render(request, 'conference_app/details.html', {'room': room})
